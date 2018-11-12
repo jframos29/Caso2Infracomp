ba6965b64759fb7fa7f39d4a1ca69314695bccbf
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2018-11-11 23:18:19 -0500

    Generador y cliente sin seguridad funcionales.

--- Caso2Infracomp-master/Caso2/src/Caso2/ClienteSS.java
+++ Caso2Infracomp-master/Caso2/src/Caso2/ClienteSS.java
@@ -18,213 +18,213 @@ import java.security.cert.X509Certificate;
 
 import org.bouncycastle.util.encoders.Hex;
 import uniandes.gload.core.Task;
+import uniandes.gload.examples.clientserver.Client;
 
 
 public class ClienteSS extends Task {
 
 
-	public static final String HOLA = "HOLA";
-	public static final String OK = "OK";
-	public static final String AlGORITMOS = "ALGORITMOS";
-	public static final String ERROR = "ERROR";
-	public static final String SEPARADOR = ":";
+    public static final String HOLA = "HOLA";
+    public static final String OK = "OK";
+    public static final String AlGORITMOS = "ALGORITMOS";
+    public static final String ERROR = "ERROR";
+    public static final String SEPARADOR = ":";
 
 
-
-	public static final String[] ALGS_SIMETRICOS = {"AES","Blowfish"};
-	public static final String[] ALGS_ASIMETRICOS = {"RSA"};
-	public static final String[] ALGS_HMAC = {"HMACMD5","HMACSHA1","HMACSHA256"};
-
-
-	private Socket socketCliente;
+    public static final String[] ALGS_SIMETRICOS = {"AES", "Blowfish"};
+    public static final String[] ALGS_ASIMETRICOS = {"RSA"};
+    public static final String[] ALGS_HMAC = {"HMACMD5", "HMACSHA1", "HMACSHA256"};
 
 
-	private Scanner sc;
-
-	private BufferedReader reader;
-
-	private PrintWriter writer;
-
-	private Seguridad seguridad;
-
-	public static Long tiempoVerificacion = 0L;
-
-	public static Long tiempoRespuesta	= 0L;
-
-	public static int numPerdidas=0;
-
-	public static Long cpu= 0L;
-
-	public static Long t1, t2, t3, t4;
+    private Socket socketCliente;
 
 
-	public ClienteSS(){
-		try{
-			System.out.println("----------------Caso 2 - Infraestructura Computacional----------------");
-			System.out.println("Integrantes:\nSergio C�rdenas 201613444, Juan Felipe Ramos 201616932, Maria Alejandra Abril 201530720");
-			sc = new Scanner(System.in);
-			seguridad = new Seguridad();
+    private Scanner sc;
 
-			System.out.println("Ingrese el puerto al que se quiere conectar:\n ");
-			int puerto = sc.nextInt();
-			System.out.println("\nListado de algoritmos disponibles:\n ");
+    private BufferedReader reader;
 
-			System.out.println("Algoritmos Sim�tricos:");
-			for(int i = 0 ; i< ALGS_SIMETRICOS.length; i++){
-				System.out.println(ALGS_SIMETRICOS[i]);
-			}
-			System.out.println("\nAlgoritmos Asim�tricos");
-			for(int i = 0 ; i< ALGS_ASIMETRICOS.length; i++){
-				System.out.println(ALGS_ASIMETRICOS[i]);
-			}
-			System.out.println("\nAlgoritmos de Hash");
-			for(int i = 0; i<ALGS_HMAC.length;i++){
-				System.out.println(ALGS_HMAC[i]);
-			}
-			System.out.println("\nIngrese los algoritmos que desea usar separados por comas:");
-			String algos = sc.next();
-			String[] algoritmos = algos.split(","); 
-			seguridad.SetAlgs(algoritmos);
-			socketCliente = new Socket("18.236.101.134",puerto);
-			socketCliente.setKeepAlive(true);
-			writer = new PrintWriter(socketCliente.getOutputStream(), true);
-			reader = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));			
+    private PrintWriter writer;
 
-			procesar();
-			System.out.println("Tiempo verificación en millis: "+tiempoVerificacion);
-			System.out.println("Tiempo respuesta consulta en millis: "+tiempoRespuesta);
-		}
-		catch (Exception e) {
-			numPerdidas++;
+    private Seguridad seguridad;
 
-			System.out.println("Cantidad de perdidas actual: "+numPerdidas);
-			e.printStackTrace();
-		}
-		try {
-			reader.close();
-			socketCliente.close();
-			writer.close();
-			sc.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+    public static Long tiempoVerificacion = 0L;
+
+    public static Long tiempoRespuesta = 0L;
+
+    public static int numPerdidas = 0;
+
+    public static double cpu = 0;
+
+    public static Long t1, t2, t3, t4;
 
 
-	}
-	
-	public static String convertByteArrayHexa(byte[] byteArray) {
-		String out = "";
-		for (int i = 0; i < byteArray.length; i++) {
-			if ((byteArray[i] & 0xff) <= 0xf) {
-				out += "0";
-			}
-			out += Integer.toHexString(byteArray[i] & 0xff).toUpperCase();
-		}
+    public ClienteSS() {
+        try {
+            System.out.println("----------------Caso 2 - Infraestructura Computacional----------------");
+            System.out.println("Integrantes:\nSergio C�rdenas 201613444, Juan Felipe Ramos 201616932, Maria Alejandra Abril 201530720");
+            sc = new Scanner(System.in);
+            seguridad = new Seguridad();
+            System.out.println("Ingrese el puerto al que se quiere conectar:\n ");
+            //int puerto = sc.nextInt();
+            int puerto = 10234;
+            //System.out.println("\nListado de algoritmos disponibles:\n ");
 
-		return out;
-	}
+            //System.out.println("Algoritmos Sim�tricos:");
+            //for (int i = 0; i < ALGS_SIMETRICOS.length; i++) {
+            //System.out.println(ALGS_SIMETRICOS[i]);
+            //}
+            //System.out.println("\nAlgoritmos Asim�tricos");
+            //for (int i = 0; i < ALGS_ASIMETRICOS.length; i++) {
+            //System.out.println(ALGS_ASIMETRICOS[i]);
+            //}
+            //System.out.println("\nAlgoritmos de Hash");
+            //for (int i = 0; i < ALGS_HMAC.length; i++) {
+            //System.out.println(ALGS_HMAC[i]);
+            //}
+            //System.out.println("\nIngrese los algoritmos que desea usar separados por comas:");
+            //String algos = sc.next();
+            //String[] algoritmos = algos.split(",");
+            String[] algoritmos = {"AES", "RSA", "HMACMD5"};
+            seguridad.SetAlgs(algoritmos);
+            socketCliente = new Socket("18.236.101.134", puerto);
+            socketCliente.setKeepAlive(true);
+            writer = new PrintWriter(socketCliente.getOutputStream(), true);
+            reader = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+            procesar();
+            //System.out.println("Tiempo verificación en millis: " + tiempoVerificacion);
+            //System.out.println("Tiempo respuesta consulta en millis: " + tiempoRespuesta);
+        } catch (Exception e) {
+            numPerdidas++;
+            //System.out.println("Cantidad de perdidas actual: " + numPerdidas);
+            e.printStackTrace();
+        }
+        try {
+            reader.close();
+            socketCliente.close();
+            writer.close();
+            //sc.close();
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+    }
+
+    public static String convertByteArrayHexa(byte[] byteArray) {
+        String out = "";
+        for (int i = 0; i < byteArray.length; i++) {
+            if ((byteArray[i] & 0xff) <= 0xf) {
+                out += "0";
+            }
+            out += Integer.toHexString(byteArray[i] & 0xff).toUpperCase();
+        }
+
+        return out;
+    }
 
 
-	public void  procesar() throws Exception{
- 		boolean termino = false;
-		boolean esperando = true;
-		int estado = 0;
-		String respuesta = "";
-		String line = "";
-		byte[] buffer;
+    public void procesar() throws Exception {
+        boolean termino = false;
+        boolean esperando = true;
+        int estado = 0;
+        String respuesta = "";
+        String line = "";
+        writer.println(HOLA);
+        while (!termino) {
+            if (reader.ready()) {
+                esperando = true;
+                line = reader.readLine();
+                if (line == null || line.equals(""))
+                    continue;
+                else if (line.toLowerCase().contains(ERROR.toLowerCase()) && estado != 5) throw new Exception(line);
+                else if (line.toLowerCase().contains(OK.toLowerCase())) System.out.println("Servidor: " + line);
 
-		writer.println(HOLA);
-		while(!termino){
-			if(reader.ready()){
-				esperando = true;
-				line = reader.readLine();
-				if(line == null || line.equals(""))
-					continue;
-				else if(line.toLowerCase().contains(ERROR.toLowerCase()) && estado != 5) throw new Exception(line);
-				else if(line.toLowerCase().contains(OK.toLowerCase())) System.out.println("Servidor: " + line);
+                switch (estado) {
+                    case 0:
+                        if (line.equals(OK)) {
+                            System.out.println("INICIANDO");
+                            respuesta = AlGORITMOS;
+                            respuesta += seguridad.darAlgos();
+                            writer.println(respuesta);
+                            estado = 1;
+                        }
+                        break;
+                    case 1:
+                        if (line.equals(OK)) {
+                            System.out.println("Se intercambiar� el Certificado Digital");
+                            seguridad.setLlaveAsimetrica();
+                            java.security.cert.X509Certificate certi = seguridad.crearCertificado();
+                            byte[] bytesCertiPem = certi.getEncoded();
+                            String certiString = new String(Hex.toHexString(bytesCertiPem));
+                            String certiFinal = certiString;
+                            writer.println(certiFinal);
+                            estado = 2;
+                        }
+                        break;
+                    case 2:
+                        if (!line.equals(OK)) {
+                            System.out.println("Se recibi� el Certificado Digital del Servidor");
+                            //System.out.println(line);
+                            System.out.println("Procesando certificado...");
+                            writer.println(OK);
+                            t1 = System.currentTimeMillis();
+                            estado = 3;
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Llave secreta recibida");
+                        System.out.println("Enviando llave secreta...");
+                        writer.println(line);
+                        estado = 4;
 
-				switch(estado){
-				case 0:
-					if(line.equals(OK)){
-						System.out.println("INICIANDO");
-						respuesta = AlGORITMOS;
-						respuesta+= seguridad.darAlgos();
-						writer.println(respuesta);
-						estado=1;
-					}
-					break;
-				case 1:
-					if(line.equals(OK)) {
-						System.out.println("Se intercambiar� el Certificado Digital");
-						seguridad.setLlaveAsimetrica();
-						java.security.cert.X509Certificate certi = seguridad.crearCertificado();
-						byte[] bytesCertiPem = certi.getEncoded();
-						String certiString = new String(Hex.toHexString(bytesCertiPem));
-						String certiFinal = certiString;
-						writer.println(certiFinal);
-						
-						estado = 2;
-					}
-					break;
-				case 2:
-					if(!line.equals(OK)) {
-						System.out.println("Se recibi� el Certificado Digital del Servidor");
-						System.out.println(line);
-						System.out.println("Procesando certificado...");
-						
-						writer.println(OK);
-						t1 = System.currentTimeMillis();
-						estado = 3;
-					}
-					break;
-				case 3:
-					System.out.println("Llave secreta recibida");
-					System.out.println("Enviando llave secreta...");
-					writer.println(line);
-					
-					estado = 4;
-					
-					break;
-				case 4:
-					if(line.equals(OK)) {
-						t2 = System.currentTimeMillis();
-						tiempoVerificacion+= t2-t1;
-						System.out.println("Ingrese su identificador de acceso:");
-						String id = sc.nextInt()+"";
-						writer.println(id);
-						t3=System.currentTimeMillis();
-						writer.println(id);
-						estado = 5;
-					}
-					
-					break;
-					
-				case 5:
-					t4=System.currentTimeMillis();
-					tiempoRespuesta+=t4-t3;
-					if(line.contains(OK)) {
-						System.out.println("Estado: "+line.split(":")[1]);
-					}
-					else
-						System.out.println("Hubo un error al realizar la consulta: "+line);
-					line = reader.readLine();
-					cpu+=Long.parseLong(line);
-					termino = true;
-					
-				default: 
-					estado = 0;
-					break;
-				}
-			}else{
-				if(esperando){
-					System.out.println("waiting...");
-					esperando = false;
-				}
-			}
-		}
+                        break;
+                    case 4:
+                        if (line.equals(OK)) {
+                            t2 = System.currentTimeMillis();
+                            tiempoVerificacion += t2 - t1;
+                            //System.out.println("Ingrese su identificador de acceso:");
+                            //String id = sc.nextInt()+"";
+                            String id = "1";
+                            writer.println(id);
+                            t3 = System.currentTimeMillis();
+                            writer.println(id);
+                            estado = 5;
+                        }
 
-	}
+                        break;
+
+                    case 5:
+                        t4 = System.currentTimeMillis();
+                        tiempoRespuesta += t4 - t3;
+                        if (line.contains(OK)) {
+                            System.out.println("Estado: "+line.split(":")[1]);
+                        }
+                        else {
+                            System.out.println("Hubo un error al realizar la consulta: " + line);
+                        }
+                        writer.println("CPU");
+                        estado=6;
+                        break;
+
+                    case 6:
+                        if(line.contains(OK)){
+                            String hola =line.split(":")[1];
+                            cpu += Double.parseDouble(hola);
+                        }
+                        termino = true;
+                        break;
+
+                    default:
+                        estado = 0;
+                        break;
+                }
+            } else {
+                if (esperando) {
+                    //System.out.println("waiting...");
+                    esperando = false;
+                }
+            }
+        }
+
+    }
 
     @Override
     public void fail() {
@@ -241,10 +241,10 @@ public class ClienteSS extends Task {
     @Override
     public void execute() {
         // TODO Auto-generated method stub
-        ClienteSS c =new ClienteSS();
+        ClienteSS c = new ClienteSS();
     }
 
-	public static void main(String[] args) {
-		new ClienteSS();
-	}
+    public static void main(String[] args) {
+        ClienteSS c = new ClienteSS();
+    }
 }
